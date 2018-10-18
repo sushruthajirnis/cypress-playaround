5fdd7d40aa5881215ce24550266458d305765861
Sushrut Hajirnis <sushruthajirnis@pop-os.localdomain>
2018-10-18 15:50:25 -0700

    clearer comment

--- cypress/integration/ius-widget/intuit-widget_update_user.js
+++ cypress/integration/ius-widget/intuit-widget_update_user.js
@@ -9,7 +9,7 @@ describe("Intuit update user ",()=>{
       })
 
       //failing test bug.....
-      // it feels as though the 
+      // it feels as though the password updates with a blank
     it("Do  update user password,check save button disabled at the begining",()=>{
         cy.get('span[class=ius-sidenav-content]').contains('Security').click()
         cy.get("#ius-password-manager-btn-update").click()
